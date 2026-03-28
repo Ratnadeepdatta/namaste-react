66695085ed881e7f6c0c9cb31e9a45308e57bcde
Ratnadeep Datta <ratnadeepdatta19996@gmail.com>
2026-03-28 23:35:15 +0530

    Install React, React Dom, Package.json Package-lock.json

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
 // const heading = React.createElement("h1", 
 //     {id:"heading"},
 //      "Hello World From React");
-     
-const heading = React.createElement("div", {id:"parent"},[
-     React.createElement("div", {id: "child"}, 
-    [ React.createElement("h1", {}, "This is nested heading"),
-        React.createElement("h2", {}, "This  nested FOR Multipull Elements")]),
 
-        React.createElement("div", {id: "child2"}, 
-    [ React.createElement("h1", {}, "This is h1 heading"),
-        React.createElement("h2", {}, "This  nested FOR Multipull Elements")])
+
+import React from "react";
+// import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+
+// const heading = React.createElement("div", { id: "parent" }, [
+//   React.createElement(
+//     "div",
+//     { id: "child", key: "child1" },  
+//     [
+//       React.createElement("h1", { key: "h1-1" }, "This is nested heading"),
+//       React.createElement("h2", { key: "h2-1" }, "This nested FOR Multipull Elements")
+//     ]
+//   ),
+
+//   React.createElement(
+//     "div",
+//     { id: "child2", key: "child2" },
+//     [
+//       React.createElement("h1", { key: "h1-2" }, "This is h1 heading"),
+//       React.createElement("h2", { key: "h2-2" }, "This nested FOR Multipull Elements")
+//     ]
+//   )
+// ]);
+const heading = React.createElement("div", {id:"parent"},[
+     React.createElement("div", {id: "child", key: "child1"}, 
+    [ React.createElement("h1", {key: "h1-1"}, "This is nested heading"),
+        React.createElement("h2", {key: "h2-1"}, "This  nested FOR Multipull Elements")]),
+
+        React.createElement("div", {id: "child2", key: "child2"}, 
+    [ React.createElement("h1", {key: "h1-2"}, "This is h1 heading"),
+        React.createElement("h2", {key: "h2-2"}, "This  nested FOR Multipull Elements")])
 ]);     
 
 
